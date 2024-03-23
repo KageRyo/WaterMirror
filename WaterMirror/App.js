7d673676465cc5fa0f1283a04b62e6faabe8fa11
KageRyo <k6.developer@gmail.com>
2024-03-23 21:38:35 +0800

    feat: disable hidden StatusBar.

--- WaterMirror/App.js
+++ WaterMirror/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './src/Home'; // 首頁
-import WaterQualityScreen from './src/Calc'; // 水質資料填寫頁面
+import HomeScreen from './src/Home';  // 首頁
+import WaterQualityScreen from './src/Calc';  // 水質資料填寫頁面
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar hidden={false} />
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: '首頁' }} />
         <Stack.Screen name="Calc" component={WaterQualityScreen} options={{ title: '水質資料填寫' }} />
