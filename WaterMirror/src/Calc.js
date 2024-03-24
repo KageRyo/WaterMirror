a5dbb4ecab919665b38afffece54977ed51b2bf9
KageRyo <k6.developer@gmail.com>
2024-03-25 00:27:23 +0800

    style: modify code formatting.

--- WaterMirror/src/Calc.js
+++ WaterMirror/src/Calc.js
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
 import { Button, Keyboard, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View, Alert, ScrollView } from 'react-native';
 
-// 水質輸入部分組件
-const WaterQualityInput = ({ label, value, onChangeText }) => (
+// 水質輸入元件
+const Input = ({ label, value, onChangeText }) => (
   <TextInput style={styles.input} placeholder={label} keyboardType="numeric" value={value} onChangeText={onChangeText} />
 );
 
-// 額外信息組件
+// 補充訊息元件
 const AdditionalInfo = ({ onPress }) => (
   <Text style={styles.additionalInfo} onPress={onPress}>
     我找不到我的水質項目
   </Text>
 );
 
+// 畫面視窗
 export default function CalcScreen() {
-  const [waterQualityData, setWaterQualityData] = useState({
+  const [data, setData] = useState({
     DO: '',
     BOD: '',
     NH3N: '',
@@ -22,49 +23,53 @@ export default function CalcScreen() {
     SS: '',
   });
 
-  const handleInputChange = (key, value) => {
-    setWaterQualityData({ ...waterQualityData, [key]: value });
+  // 更新輸入資料
+  const updateInput = (key, value) => {
+    setData({ ...data, [key]: value });
   };
 
+  // 送出資料
   const handleSubmit = () => {
-    // 在這裡處理提交操作
-    const filledData = Object.keys(waterQualityData).filter(key => waterQualityData[key] !== '');
+    const filledData = Object.keys(data).filter(key => data[key] !== '');
     if (filledData.length > 0) {
       const message = `已送出${filledData.join(', ')}資料`;
       Alert.alert('提示', message, [{ text: '確定' }]);
-      console.log('客戶端傳送了水質資料', waterQualityData);
-      const csvData = `DO,BOD,NH3N,EC,SS\n${waterQualityData.DO},${waterQualityData.BOD},${waterQualityData.NH3N},${waterQualityData.EC},${waterQualityData.SS}`;
+      console.log('客戶端傳送了水質資料', data);
+      const csvData = `DO,BOD,NH3N,EC,SS\n${data.DO},${data.BOD},${data.NH3N},${data.EC},${data.SS}`;
       console.log(csvData);
     } else {
       Alert.alert('提示', '請填寫至少一項水質資料', [{ text: '確定' }]);
       console.log('客戶端未填寫水質資料');
     }
-    // 清空水質資料
-    setWaterQualityData({ DO: '', BOD: '', NH3N: '', EC: '', SS: '' });
-    // 隱藏鍵盤
+    clearInput();
+    dismissKBD();
+  };
+
+  // 清除輸入資料
+  const clearInput = () => {
+    setData({ DO: '', BOD: '', NH3N: '', EC: '', SS: '' });
+  };
+
+  // 隱藏鍵盤
+  const dismissKBD = () => {
     Keyboard.dismiss();
   };
 
-  const dismissKeyboard = () => {
-    // 隱藏鍵盤
-    Keyboard.dismiss();
-  };
-
+  // 補充訊息處理
   const handleAdditionalInfo = () => {
-    // 顯示提示信息
     Alert.alert('偷偷和你說', '不同的水質資料項目需要蒐集更多的水質資料，歡迎對此專案進行貢獻。', [{ text: '了解' }]);
   };
 
   return (
-    <TouchableWithoutFeedback onPress={dismissKeyboard}>
+    <TouchableWithoutFeedback onPress={dismissKBD}>
       <ScrollView contentContainerStyle={styles.container}>
         <View style={styles.inputContainer}>
           <Text style={styles.title}>手動輸入水質資料</Text>
-          <WaterQualityInput label="溶氧量（DO, %）" value={waterQualityData.DO} onChangeText={text => handleInputChange('DO', text)} />
-          <WaterQualityInput label="生物需氧量（BOD, mg/L）" value={waterQualityData.BOD} onChangeText={text => handleInputChange('BOD', text)} />
-          <WaterQualityInput label="懸浮固體（SS, mg/L）" value={waterQualityData.SS} onChangeText={text => handleInputChange('SS', text)} />
-          <WaterQualityInput label="氨氮（NH3-N, mg/L）" value={waterQualityData.NH3N} onChangeText={text => handleInputChange('NH3N', text)} />
-          <WaterQualityInput label="導電度（EC, μumho/co）" value={waterQualityData.EC} onChangeText={text => handleInputChange('EC', text)} />
+          <Input label="溶氧量（DO, %）" value={data.DO} onChangeText={text => updateInput('DO', text)} />
+          <Input label="生物需氧量（BOD, mg/L）" value={data.BOD} onChangeText={text => updateInput('BOD', text)} />
+          <Input label="懸浮固體（SS, mg/L）" value={data.SS} onChangeText={text => updateInput('SS', text)} />
+          <Input label="氨氮（NH3-N, mg/L）" value={data.NH3N} onChangeText={text => updateInput('NH3N', text)} />
+          <Input label="導電度（EC, μumho/co）" value={data.EC} onChangeText={text => updateInput('EC', text)} />
         </View>
 
         <View style={styles.dataContainer}>
@@ -72,7 +77,7 @@ export default function CalcScreen() {
           {/* 在這裡顯示輸入的水質資料 */}
         </View>
 
-        <View style={styles.buttonContainer}>
+        <View style={styles.btnContainer}>
           <Button title="新增一筆" onPress={() => {}} />
           <Button title="修改" onPress={() => {}} />
           <Button title="刪除" onPress={() => {}} />
@@ -81,7 +86,7 @@ export default function CalcScreen() {
 
         <View style={styles.separator} />
 
-        <View style={styles.buttonContainer}>
+        <View style={styles.btnContainer}>
           <Button title="上傳水質資料檔案" onPress={() => {}} />
           <Button title="與自動化設備連線" onPress={() => {}} />
         </View>
@@ -92,6 +97,7 @@ export default function CalcScreen() {
   );
 }
 
+// 主樣式表
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
@@ -124,7 +130,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 10,
   },
-  buttonContainer: {
+  btnContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
     width: '80%',
