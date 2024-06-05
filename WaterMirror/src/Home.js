eefd6cfa05b49159466edfd746e6565b09f420ea
Ryo <k6.developer@gmail.com>
2024-06-05 14:50:56 +0800

    fix: #13 message modify.

--- WaterMirror/src/Home.js
+++ WaterMirror/src/Home.js
@@ -12,7 +12,7 @@ const TopSection = () => {
     <View style={topStyles.top}>
       <Text style={topStyles.title}>WaterMirror</Text>
       <Text style={topStyles.subtitle}>智慧化水質分析與評估系統</Text>
-      <Text style={topStyles.platform}>版本：WaterMirror-{Platform.OS}-v1.0.7</Text>
+      <Text style={topStyles.platform}>版本：WaterMirror-{Platform.OS}-v1.0.8</Text>
     </View>
   );
 };
