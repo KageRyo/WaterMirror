6c0c0703608e727a0706ad5abfc30a1997969df7
kageryo <kageryo@coderyo.com>
2026-05-19 17:01:59 +0800

    docs(config): align default backend URL

--- src/utils/appConfig.cjs
+++ src/utils/appConfig.cjs
@@ -1,5 +1,5 @@
 function normalizeBaseUrl(url) {
-  return (url || 'http://127.0.0.1:8011').replace(/\/+$/, '');
+  return (url || 'http://localhost:8001').replace(/\/+$/, '');
 }
 
 function parseInteger(value, fallback) {
